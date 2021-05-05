a91f094d2c4b9edd8cdeb9ec93332f5fc49bfa50
unknown <denniswillie2000@gmail.com>
2021-05-05 21:13:01 +0100

    db connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,27 @@
 require('dotenv').config({
     path: __dirname + '/../../.env'
 });
+const inProduction = process.env.NODE_ENV === "production";
+
 const Pool = require('pg').Pool;
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-})
+
+let pool;
+
+if (inProduction) {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    })
+} else {
+    pool = new Pool({
+        user: process.env.POSTGRES_USER,
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_DATABASE,
+        password: process.env.POSTGRES_PASSWORD,
+        port: process.env.POSTGRES_PORT,
+    })
+}
 
 module.exports = pool;
