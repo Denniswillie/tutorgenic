ed63e45dae8799847a91cfb099f1214115c27943
unknown <denniswillie2000@gmail.com>
2021-05-06 23:05:10 +0100

    fsd

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -56,12 +56,12 @@ export default function ApplyTutor(props) {
                                 if (msg.destination == user._id && msg.from === newClientId) {
                                     var answered = false;
                                     const remoteStream = new MediaStream();
-                                    setRemoteStreams(prevData => {
-                                        return [...prevData, remoteStream];
-                                    })
-                                    // const temp = document.createElement('video');
-                                    // temp.srcObject = remoteStream;
-                                    // document.getElementById('videos').appendChild(temp);
+                                    // setRemoteStreams(prevData => {
+                                    //     return [...prevData, remoteStream];
+                                    // })
+                                    const temp = document.createElement('video');
+                                    temp.srcObject = remoteStream;
+                                    document.getElementById('videos').appendChild(temp);
 
                                     const localIceCandidates = [];
 
@@ -134,12 +134,12 @@ export default function ApplyTutor(props) {
                                     })
 
                                     const remoteStream = new MediaStream();
-                                    setRemoteStreams(prevData => {
-                                        return [...prevData, remoteStream];
-                                    })
-                                    // const temp = document.createElement('video');
-                                    // temp.srcObject = remoteStream;
-                                    // document.getElementById('videos').appendChild(temp);
+                                    // setRemoteStreams(prevData => {
+                                    //     return [...prevData, remoteStream];
+                                    // })
+                                    const temp = document.createElement('video');
+                                    temp.srcObject = remoteStream;
+                                    document.getElementById('videos').appendChild(temp);
                                     peerConnection.addEventListener('track', event => {
                                         event.streams[0].getTracks().forEach(track => {
                                             remoteStream.addTrack(track);
@@ -212,6 +212,13 @@ export default function ApplyTutor(props) {
         }
     }, [setUser])
 
+    // {remoteStreams.map(remoteStream => {
+    //     console.log('remote');
+    //     return <div>
+    //         <video ref={video => {video.srcObject = remoteStream;}} autoPlay playsInLine></video><br />
+    //     </div>;
+    // })}
+
     return <div className="wrapper">
         <div className="content" style={{flex: "0.75"}}>
 
@@ -219,11 +226,9 @@ export default function ApplyTutor(props) {
         <div className="sidebar" style={{flex: "0.25", overflow: "auto"}}>
             <video id="localVideo" muted autoPlay playsInLine></video><br />
             
-            {remoteStreams.map(remoteStream => {
-                return <div>
-                    <video ref={video => {video.srcObject = remoteStream;}} autoPlay playsInLine></video><br />
-                </div>;
-            })}
+            <div id="videos">
+
+            </div>
         </div>
     </div>
 }
